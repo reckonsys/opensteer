b6179bce9f1cd5e3bdf77dbd4499b822a01367f4
dhilipsiva <dhilipsiva@pm.me>
2019-09-20 18:37:54 +0530

    Show list of organizations

--- opensteer/contrib/views.py
+++ opensteer/contrib/views.py
@@ -1,5 +1,13 @@
+from django.db.models import Q
 from django.shortcuts import render
+
+from opensteer.teams.models import Organization
 
 
 def index(request):
-    return render(request, 'index.html')
+    organizations = None
+    if request.user.is_authenticated:
+        organizations = Organization.objects.filter(
+            Q(staff__user__id=request.user.id) | Q(owner__id=request.user.id))
+    ctx = {'organizations': organizations}
+    return render(request, 'index.html', ctx)
