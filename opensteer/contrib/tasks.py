26c268902802baed9b8dd3096123db4cb55fafe2
dhilipsiva <dhilipsiva@pm.me>
2019-09-24 12:11:46 +0530

    Logic to create standups

--- opensteer/contrib/tasks.py
+++ opensteer/contrib/tasks.py
@@ -1,9 +1,12 @@
+from django.utils import timezone
 from django.contrib.auth import get_user_model
+
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
-# from config import celery_app
+from opensteer.teams.models import Organization
+
 
 User = get_user_model()
 logger = get_task_logger(__name__)
@@ -15,14 +18,20 @@ logger = get_task_logger(__name__)
     ignore_result=True
 )
 def create_meetings():
-    # @celery_app.task()
     """A pointless Celery task to demonstrate usage."""
-    u = User.objects.count()
-    logger.info(u)
-    print(u)
-    return u
+    now = timezone.now()
+    for organization in Organization.objects.filter(
+            meeting_hour=now.hour, meeting_minute=now.minute):
+        logger.info(f'Attempting to create meetings for {organization.name}')
 
-
-rom django.utils import timezone
-
-now = timezone.now()
+        for team in organization.teams.all():
+            if team.standups.filter(date=now.date()).count() == 0:
+                # We don't have any standups, yet! so lets create one.
+                # First, deactivate all old standups
+                # TODO: Send emails to Admins & managers stating stale Standups
+                # Stale Standups: The ones that are missing responses
+                team.standups.filter(is_active=True).update(is_active=False)
+                logger.info(f'New Standup for {team.name}')
+                standup = team.standups.create(date=now.date())
+                # Send notifications to all members of organization
+                logger.info(standup)
