25cfe68530d84a6a714862de1207160e165c67ad
dhilipsiva <dhilipsiva@pm.me>
2019-10-02 12:01:13 +0530

    Remove stale app

--- opensteer/checkins/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from opensteer.checkins.models import Checkin, CheckinResponse
-
-admin.site.register(Checkin)
-admin.site.register(CheckinResponse)
--- opensteer/checkins/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CheckinsConfig(AppConfig):
-    name = 'opensteer.checkins'
